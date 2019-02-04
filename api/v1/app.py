bec63d8020c982eaeaa6a92db88a2065d6296da7
thenicopixie <391@holbertonschool.com>
2019-02-04 23:21:29 +0000

    create app variable which is an instance of Flask. import storage. Register the app_views blueprint. Declare a app.teardown to call storage.close

---
+++ api/v1/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Returns the status of the API"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = flask.Flask(__name__)
+
+app_views = register_blueprint('app_views')
+
+@app.teardown_appcontext
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', threaded=True)
