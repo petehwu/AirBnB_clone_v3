b26a89c67ce7e80691f6a8c96a3a00ed2ac53f0a
thenicopixie <391@holbertonschool.com>
2019-02-05 22:00:56 +0000

    add delete and post methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,9 +5,10 @@ from flask import jsonify
 from flask import abort
 from api.v1.views import app_views
 from models import storage
+from flask import request
+import jsonify
 
-
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes = False)
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id=None):
      states = [state.to_dict() for state in storage.all("State").values()]
@@ -18,5 +19,25 @@ def get_state(state_id=None):
          if obj is None:
              abort(404)
          return jsonify(obj.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id=None):
+    state = storage.get("State", state_id)
+    print(state)
+    if state is None:
+        abort(404)
+    else:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+
+@app_views.route('/api/v1/states', methods=['POST'])
+def post_state():
+    data = request.get_json()
+    if not data.validate():
+        abort(400, 'Not a JSON')
+    if name not in data:
+        abort(400, 'Missing name')
+    return jsonify(data), 201
 
 
