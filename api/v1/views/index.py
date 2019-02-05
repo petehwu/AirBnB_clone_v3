e7067cbe4c900d81d599fc25cf97ea4f329deb6e
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-04 16:14:04 -0800

    added index.py file

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status')
+def status():
+    """returns a json ok string
+    """
+    return jsonify({"status":"OK"})
+
