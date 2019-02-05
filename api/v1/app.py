bdc8c050b5c9c6460354f9e42c5be79a93afff66
thenicopixie <391@holbertonschool.com>
2019-02-05 00:10:52 +0000

    handler for 404 error. Returns a JSON 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,9 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
+from flask import make_response
+
 
 app = Flask(__name__)
 
@@ -13,6 +16,11 @@ app.url_map.strict_slashes = False
 @app.teardown_appcontext
 def storage_close(value):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', threaded=True)
