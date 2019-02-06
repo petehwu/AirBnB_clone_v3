8f494525b96cd2582e5582604e0928fdca67317e
thenicopixie <391@holbertonschool.com>
2019-02-06 03:14:57 +0000

    add import for amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,6 +9,8 @@ from api.v1.views import places
 from api.v1.views import places_reviews"""
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views import cities
+if app_views:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views import cities
+    from api.v1.views import amenities
